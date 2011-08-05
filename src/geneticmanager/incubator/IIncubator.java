a5de2545751b54d2dda350af56e4a3a6c0b2b9e9
Matthieu Vergne <sazaju@gmail.com>
2011-08-05 13:51:53 +0200

    Some corrections.

--- src/geneticmanager/incubator/IIncubator.java
+++ src/geneticmanager/incubator/IIncubator.java
@@ -1,25 +1,12 @@
 package geneticmanager.incubator;
 
 import geneticmanager.individual.IIndividual;
-import geneticmanager.individual.IIndividualReproducer;
 
 //TODO tests
 //TODO javadoc
-//TODO simplify the crossPopulation+getReproducer feature
-public interface IIncubator<Indiv extends IIndividual<Gene>, Gene> {
+//TODO see what can be considered as generic enough to be asked here
+public interface IIncubator<Indiv extends IIndividual<?>> {
 	public void addIndividual(Indiv individual);
 
 	public Indiv getBestIndividual();
-
-	public void crossPopulation();
-
-	public void makeSelection();
-
-	public long getGenerationCounter();
-
-	public int getStationaryTime();
-
-	public int getGenerationSize();
-
-	public IIndividualReproducer<Indiv> getReproducer();
 }
--- src/sample/TSP/Incubator.java
+++ src/sample/TSP/Incubator.java
@@ -1,9 +1,17 @@
 package sample.TSP;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 import geneticmanager.individual.IIndividualReproducer;
 
 public class Incubator extends
-		geneticmanager.incubator.DefaultIncubator<Individual, Integer> {
+		geneticmanager.incubator.DefaultIncubator<Individual> {
+	private long generationCounter = 0;
+	private final ArrayList<Individual> population = new ArrayList<Individual>();
+	private int stationaryTime = 0;
+	private boolean isDoubleKept = true;
 	private IndividualFactory factory;
 	private IIndividualReproducer<Individual> reproducer = new IIndividualReproducer<Individual>() {
 
@@ -13,7 +21,6 @@ public class Incubator extends
 		}
 	};
 
-	@Override
 	public IIndividualReproducer<Individual> getReproducer() {
 		return reproducer;
 	}
@@ -24,6 +31,100 @@ public class Incubator extends
 
 	public void setFactory(IndividualFactory factory) {
 		this.factory = factory;
+	}
+
+	public void crossPopulation() {
+		if (reproducer == null) {
+			throw new IllegalStateException(
+					"No reproducer as been defined, you cannot cross the population.");
+		}
+		ArrayList<Individual> availableIndividuals = new ArrayList<Individual>(
+				population);
+		while (availableIndividuals.size() > 1) {
+			Individual i1 = availableIndividuals.remove(Util
+					.randomIndex(availableIndividuals.size()));
+			Individual i2 = availableIndividuals.remove(Util
+					.randomIndex(availableIndividuals.size()));
+			population.add(reproducer.reproduce(i1, i2));
+		}
+
+		generationCounter++;
+	}
+
+	public long getGenerationCounter() {
+		return generationCounter;
+	}
+
+	private void removeDoubles() {
+		ArrayList<Individual> temp = new ArrayList<Individual>(population);
+		for (Individual i1 : temp) {
+			for (Individual i2 : temp) {
+				if (i1 != i2 && Arrays.deepEquals(i1.getGenes(), i2.getGenes())) {
+					population.remove(i2);
+				}
+			}
+		}
+	}
+
+	private void reducePopulation() {
+		Collections.sort(population);
+		while (population.size() > getGenerationSize()) {
+			population.remove(getGenerationSize());
+		}
+	}
+
+	public void makeSelection() {
+		Individual previousResult = getBestIndividual();
+
+		if (!isDoubleKept()) {
+			removeDoubles();
+		}
+		reducePopulation();
+
+		Individual newResult = getBestIndividual();
+		if (newResult.compareTo(previousResult) != 0) {
+			stationaryTime = 0;
+		} else {
+			stationaryTime++;
+		}
+	}
+
+	private int generationSize = 10;
+
+	public int getGenerationSize() {
+		return generationSize;
+	}
+
+	public void setGenerationSize(int generationSize) {
+		this.generationSize = generationSize;
+	}
+
+	@Override
+	public Individual getBestIndividual() {
+		return population.isEmpty() ? null : population.get(0);
+	}
+
+	@Override
+	public void addIndividual(Individual individual) {
+		population.add(individual);
+	}
+
+	public int getStationaryTime() {
+		return stationaryTime;
+	}
+
+	public void clean() {
+		population.clear();
+		stationaryTime = 0;
+		generationCounter = 0;
+	}
+
+	public boolean isDoubleKept() {
+		return isDoubleKept;
+	}
+
+	public void setDoubleKept(boolean isDoubleKept) {
+		this.isDoubleKept = isDoubleKept;
 	}
 
 }
